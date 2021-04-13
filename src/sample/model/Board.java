32eec93421b908aa0452cde7b255dcea64d7546e
Miguel <rawredonion@gmail.com>
2021-04-13 13:12:48 -0400

    Added some touch ups and comments.

--- src/sample/model/Board.java
+++ src/sample/model/Board.java
@@ -4,12 +4,16 @@ import sample.utility.Orientation;
 import sample.utility.ShipType;
 
 public class Board {
-    Cell[][] grid = new Cell[10][10];
+    final static int MAX_ROW = 10;
+    final static int MAX_COL = 10;
+    Cell[][] grid = new Cell[MAX_ROW][MAX_COL]; // Grid holding cells for game
 
+    // Initialize our grid
     public Board() {
         initGrid();
     }
 
+    // place empty cells throughout our grid with respective coordinates
     void initGrid() {
         for(int y = 0; y < 10; ++y) {
             for(int x = 0; x < 10; ++x) {
@@ -17,7 +21,7 @@ public class Board {
             }
         }
     }
-
+    // Return the respective cell based on x, y coordinate
     public Cell getCellByCoord(int x, int y){
         return grid[y - 1][x - 1];
     }
