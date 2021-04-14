81ddd6a0cc2a60fb85059e2d383c0d954ffb218f
tylermihelich <tpm18b@my.fsu.edu>
2021-04-13 20:55:35 -0400

    changed load

--- src/sample/model/Load.java
+++ src/sample/model/Load.java
@@ -2,9 +2,11 @@ package sample.model;
 
 import com.google.gson.Gson;
 
+import java.io.File;
+
 public class Load
 {
-    public static void loadGame() {
+    public static void loadGame(File game) {
         Gson gson = new Gson();
         System.out.println(gson);
     }
