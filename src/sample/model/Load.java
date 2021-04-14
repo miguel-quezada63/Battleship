9c5f682e10145a7488a281b083d2a7ebbca8d3c8
tylermihelich <tpm18b@my.fsu.edu>
2021-04-14 11:23:37 -0400

    changed load

--- src/sample/model/Load.java
+++ src/sample/model/Load.java
@@ -2,13 +2,22 @@ package sample.model;
 
 import com.google.gson.Gson;
 
-import java.io.File;
+import java.io.*;
 
 public class Load
 {
     public static void loadGame(File game) {
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(game));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
         Gson gson = new Gson();
-        System.out.println(gson);
+        Object json = gson.fromJson(bufferedReader, Object.class);
+
+        System.out.println(json.toString());
     }
 }
 
