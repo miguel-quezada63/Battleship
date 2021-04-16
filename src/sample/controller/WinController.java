b70642137327789541fb651c556246b43605911d
adj18 <59737265+adj18@users.noreply.github.com>
2021-04-16 10:28:49 -0400

    added a win.fxml file

---
+++ src/sample/controller/WinController.java
@@ -0,0 +1,42 @@
+package sample.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class WinController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Pane gameWinPane;
+
+    @FXML
+    private Button playAgainBtn;
+
+    @FXML
+    private Button quitGameButton;
+
+    @FXML
+    void playAgain(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quitGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
