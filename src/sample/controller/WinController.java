8ed62bb40966bb23b552d2690a0a9e77c1a4fa61
adj18 <59737265+adj18@users.noreply.github.com>
2021-04-16 12:45:33 -0400

    added a win.fxml file

--- src/sample/controller/WinController.java
+++ src/sample/controller/WinController.java
@@ -1,11 +1,16 @@
 package sample.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class WinController {
 
@@ -25,18 +30,27 @@ public class WinController {
     private Button quitGameButton;
 
     @FXML
-    void playAgain(ActionEvent event) {
-
+    public void playAgain() {
+        try {
+            Parent newRoot = FXMLLoader.load(getClass().getResource("/sample/view/placement.fxml"));
+            Scene s = playAgainBtn.getScene();
+            s.setRoot(newRoot);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
     }
 
     @FXML
-    void quitGame(ActionEvent event) {
-
+    public void quitGame() {
+        Stage s = (Stage) quitGameButton.getScene().getWindow();
+        s.close();
     }
 
     @FXML
     void initialize() {
 
-
     }
+
+
+
 }
